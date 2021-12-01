6bf74d28061825d629d0ce49117da1dbaee3862d
Joel B. Mohler <joel@kiwistrawberry.us>
2021-11-30 21:28:12 -0500

    update to use request & response api from yenot; formatting

--- bitserver/bits.py
+++ bitserver/bits.py
@@ -1,20 +1,22 @@
 import uuid
-from bottle import request
 import yenot.backend.api as api
 
 app = api.get_global_app()
 
-def get_api_databits_bits_list_prompts():
-    return api.PromptList(
-            frag=api.cgen.basic(label='Search'),
-            __order__=['frag'])
 
-@app.get('/api/databits/bits/list', name='get_api_databits_bits_list', \
-        report_prompts=get_api_databits_bits_list_prompts,
-        report_title='Data Bit List')
-def get_api_databits_bits_list():
-    frag = request.query.get('frag', None)
-    tag = request.query.get('tag_id', None)
+def get_api_databits_bits_list_prompts():
+    return api.PromptList(frag=api.cgen.basic(label="Search"), __order__=["frag"])
+
+
+@app.get(
+    "/api/databits/bits/list",
+    name="get_api_databits_bits_list",
+    report_prompts=get_api_databits_bits_list_prompts,
+    report_title="Data Bit List",
+)
+def get_api_databits_bits_list(request):
+    frag = request.query.get("frag", None)
+    tag = request.query.get("tag_id", None)
 
     select = """
 select bits.id, bits.caption, bits.data, bits.website
@@ -25,12 +27,14 @@ where /*WHERE*/
 
     params = {}
     wheres = []
-    if frag != None and frag != '':
-        params['frag'] = api.sanitize_fts(frag)
+    if frag != None and frag != "":
+        params["frag"] = api.sanitize_fts(frag)
         wheres.append("fts.fts_search @@ to_tsquery(%(frag)s)")
     if tag != None:
-        params['tag'] = tag
-        wheres.append("(select count(*) from databits.tagbits where tag_id=%(tag)s and bit_id=bits.id)>0")
+        params["tag"] = tag
+        wheres.append(
+            "(select count(*) from databits.tagbits where tag_id=%(tag)s and bit_id=bits.id)>0"
+        )
 
     if len(wheres) == 0:
         wheres.append("True")
@@ -38,11 +42,13 @@ where /*WHERE*/
 
     results = api.Results(default_title=True)
     with app.dbconn() as conn:
-        cm = api.ColumnMap(\
-                id=api.cgen.lms_bits_persona.surrogate(),
-                l_name=api.cgen.lms_bits_persona.name(url_key='id', represents=True))
-        results.tables['bits', True] = api.sql_tab2(conn, select, params, cm)
+        cm = api.ColumnMap(
+            id=api.cgen.lms_bits_persona.surrogate(),
+            l_name=api.cgen.lms_bits_persona.name(url_key="id", represents=True),
+        )
+        results.tables["bits", True] = api.sql_tab2(conn, select, params, cm)
     return results.json_out()
+
 
 def _get_api_databits_bit(a_id=None, newrow=False):
     select = """
@@ -53,7 +59,7 @@ where /*WHERE*/"""
     wheres = []
     params = {}
     if a_id != None:
-        params['i'] = a_id
+        params["i"] = a_id
         wheres.append("bits.id=%(i)s")
     if newrow:
         wheres.append("False")
@@ -66,46 +72,58 @@ where /*WHERE*/"""
         columns, rows = api.sql_tab2(conn, select, params)
 
         if newrow:
+
             def default_row(index, row):
                 row.id = str(uuid.uuid1())
+
             rows = api.tab2_rows_default(columns, [None], default_row)
 
-        results.tables['bit', True] = columns, rows
+        results.tables["bit", True] = columns, rows
     return results
 
-@app.get('/api/databits/bit/<a_id>', name='get_api_databits_bit')
+
+@app.get("/api/databits/bit/<a_id>", name="get_api_databits_bit")
 def get_api_databits_bit(a_id):
     results = _get_api_databits_bit(a_id)
     return results.json_out()
 
-@app.get('/api/databits/bit/new', name='get_api_databits_bit_new')
+
+@app.get("/api/databits/bit/new", name="get_api_databits_bit_new")
 def get_api_databits_bit_new():
     results = _get_api_databits_bit(newrow=True)
-    results.keys['new_row'] = True
+    results.keys["new_row"] = True
     return results.json_out()
 
-@app.put('/api/databits/bit/<bit_id>', name='put_api_databits_bit')
+
+@app.put("/api/databits/bit/<bit_id>", name="put_api_databits_bit")
 def put_api_databits_bit(bit_id):
-    acc = api.table_from_tab2('bit', amendments=['id'], options=['caption', 'data', 'website', 'uname', 'pword'])
+    acc = api.table_from_tab2(
+        "bit",
+        amendments=["id"],
+        options=["caption", "data", "website", "uname", "pword"],
+    )
 
     if len(acc.rows) != 1 or acc.rows[0].id != bit_id:
-        raise api.UserError('invalid-input', 'There must be exactly one row and it must match the url.')
+        raise api.UserError(
+            "invalid-input", "There must be exactly one row and it must match the url."
+        )
 
     with app.dbconn() as conn:
         with api.writeblock(conn) as w:
-            w.upsert_rows('databits.bits', acc)
+            w.upsert_rows("databits.bits", acc)
         conn.commit()
 
     return api.Results().json_out()
 
-@app.delete('/api/databits/bit/<bit_id>', name='delete_api_databits_bit')
+
+@app.delete("/api/databits/bit/<bit_id>", name="delete_api_databits_bit")
 def delete_api_databits_bit(bit_id):
     delete_sql = """
 delete from databits.bits where id=%(bid)s;
 """
 
     with app.dbconn() as conn:
-        api.sql_void(conn, delete_sql, {'bid': bit_id})
+        api.sql_void(conn, delete_sql, {"bid": bit_id})
         conn.commit()
 
     return api.Results().json_out()
--- bitserver/tags.py
+++ bitserver/tags.py
@@ -3,8 +3,12 @@ import yenot.backend.api as api
 
 app = api.get_global_app()
 
-@app.get('/api/databits/tags/list', name='get_api_databits_tags_list', \
-        report_title='Tag List')
+
+@app.get(
+    "/api/databits/tags/list",
+    name="get_api_databits_tags_list",
+    report_title="Tag List",
+)
 def get_api_databits_tags_list():
     select = """
 select *
@@ -16,11 +20,13 @@ order by name
 
     results = api.Results(default_title=True)
     with app.dbconn() as conn:
-        cm = api.ColumnMap(\
-                id=api.cgen.lms_databits_tag.surrogate(),
-                name=api.cgen.lms_databits_tag.name(url_key='id', represents=True))
-        results.tables['tags', True] = api.sql_tab2(conn, select, params, cm)
+        cm = api.ColumnMap(
+            id=api.cgen.lms_databits_tag.surrogate(),
+            name=api.cgen.lms_databits_tag.name(url_key="id", represents=True),
+        )
+        results.tables["tags", True] = api.sql_tab2(conn, select, params, cm)
     return results.json_out()
+
 
 def _get_api_databits_tag(a_id=None, newrow=False):
     select = """
@@ -31,7 +37,7 @@ where /*WHERE*/"""
     wheres = []
     params = {}
     if a_id != None:
-        params['i'] = a_id
+        params["i"] = a_id
         wheres.append("tags.id=%(i)s")
     if newrow:
         wheres.append("False")
@@ -44,46 +50,54 @@ where /*WHERE*/"""
         columns, rows = api.sql_tab2(conn, select, params)
 
         if newrow:
+
             def default_row(index, row):
                 row.id = str(uuid.uuid1())
+
             rows = api.tab2_rows_default(columns, [None], default_row)
 
-        results.tables['tag', True] = columns, rows
+        results.tables["tag", True] = columns, rows
     return results
 
-@app.get('/api/databits/tag/<a_id>', name='get_api_databits_tag')
+
+@app.get("/api/databits/tag/<a_id>", name="get_api_databits_tag")
 def get_api_databits_tag(a_id):
     results = _get_api_databits_tag(a_id)
     return results.json_out()
 
-@app.get('/api/databits/tag/new', name='get_api_databits_tag_new')
+
+@app.get("/api/databits/tag/new", name="get_api_databits_tag_new")
 def get_api_databits_tag_new():
     results = _get_api_databits_tag(newrow=True)
-    results.keys['new_row'] = True
+    results.keys["new_row"] = True
     return results.json_out()
 
-@app.put('/api/databits/tag/<tag_id>', name='put_api_databits_tag')
+
+@app.put("/api/databits/tag/<tag_id>", name="put_api_databits_tag")
 def put_api_databits_tag(tag_id):
-    tag = api.table_from_tab2('tag', amendments=['id'], options=['name', 'description'])
+    tag = api.table_from_tab2("tag", amendments=["id"], options=["name", "description"])
 
     if len(tag.rows) != 1 or tag.rows[0].id != tag_id:
-        raise api.UserError('invalid-input', 'There must be exactly one row and it must match the url.')
+        raise api.UserError(
+            "invalid-input", "There must be exactly one row and it must match the url."
+        )
 
     with app.dbconn() as conn:
         with api.writeblock(conn) as w:
-            w.upsert_rows('databits.tags', tag)
+            w.upsert_rows("databits.tags", tag)
         conn.commit()
 
     return api.Results().json_out()
 
-@app.delete('/api/databits/tag/<tag_id>', name='delete_api_databits_tag')
+
+@app.delete("/api/databits/tag/<tag_id>", name="delete_api_databits_tag")
 def delete_api_databits_tag(tag_id):
     delete_sql = """
 delete from databits.tags where id=%(tid)s;
 """
 
     with app.dbconn() as conn:
-        api.sql_void(conn, delete_sql, {'tid': tag_id})
+        api.sql_void(conn, delete_sql, {"tid": tag_id})
         conn.commit()
 
     return api.Results().json_out()
